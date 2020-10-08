17bd2b827d03ce4f68f5ee884894afd38224671d
刘丹飏 <wo993817570@163.com>
2020-10-08 09:48:22 +0800

    第二次提交

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="goods">
+    <goods-list-item  v-for="item in goods" :key="item.show.img" :goodsItem="item"></goods-list-item>
+  </div>
+</template>
+
+<script>
+import GoodsListItem from './GoodsListItem'
+export default {
+  name: 'GoodsList',
+  props: {
+    goods: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  components: {
+    GoodsListItem
+  }
+}
+</script>
+
+<style>
+  .goods {
+    display: flex;
+    justify-content: space-around;
+    padding: 2px;
+    flex-wrap: wrap;
+  }
+
+</style>
